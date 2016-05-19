a8c9b13fab5ed49f2ce079ea1fc68c0148d522af
Dan Elstone <daniel.elstone@gmail.com>
2016-05-19 11:31:37 -0700

    added flexsliders

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,57 @@
 'use strict';
  
 var gulp = require('gulp'),
-sass = require('gulp-sass'),
-autoprefixer = require('gulp-autoprefixer');
+	sass = require('gulp-sass'),
+	//uglify = require('gulp-uglify'),
+	cleanCSS = require('gulp-clean-css'),
+	autoprefixer = require('gulp-autoprefixer');
 
 //SASS task
 
 gulp.task('sass', function () {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./src/sass/**/main.scss')
     .pipe(sass.sync().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
-});
- 
-
-//autoprefixer task
-
-gulp.task('autoprefixer', function () {
-	return gulp.src('css/styles.css')
-		.pipe(autoprefixer({
+    .pipe(autoprefixer({
 			browsers: ['last 2 versions'],
 			cascade: false
 		}))
-		.pipe(gulp.dest('./css'));
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./dist/css'));
 });
+ 
+//autoprefixer task
+
+// gulp.task('autoprefixer', function () {
+// 	return gulp.src('css/styles.css')
+// 		.pipe(autoprefixer({
+// 			browsers: ['last 2 versions'],
+// 			cascade: false
+// 		}))
+// 		.pipe(gulp.dest('./css'));
+// });
 
 //live reload
 gulp.task('watch', function() {
 	
-	gulp.watch('./sass/**/*.scss', ['sass']);
+	gulp.watch('./src/sass/**/*.scss', ['sass']);
 
 });
+
+// gulp.task('compress', function() {
+//   return gulp.src('sass/styles.scss')
+//     .pipe(uglify())
+//     .pipe(gulp.dest('dist'));
+// });
+ 
+ 
+// gulp.task('minify-css', function() {
+//     return gulp.src('./styles/**/*.css')
+        
+//         .pipe(gulp.dest('dist'));
+//     });
+
+
+
+
 
 
